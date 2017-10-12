e6adb15557f6dc1b4b379aea5f25ff68e710a625
Antonín Jareš <jares.antonin@gmail.com>
2017-10-12 14:21:33 +0200

    Update tab.module.d.ts

--- dist/tab.module.d.ts
+++ dist/tab.module.d.ts
@@ -1,6 +1,14 @@
 import { NguiTabComponent } from "./tab.component";
 import { NgModule } from '@angular/core';
-export { NguiTabComponent };
-@NgModule({})
+@NgModule({
+  declarations: [
+    NguiTabComponent
+  ],
+  exports: [
+    NguiTabComponent
+  ]
+})
 export declare class NguiTabModule {
 }
+
+export { NguiTabComponent };
